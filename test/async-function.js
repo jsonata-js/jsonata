067a33667f1bd79b10507d478d9fd63b6f122409
andrew-coleman <andrew_coleman@uk.ibm.com>
2017-10-18 13:49:12 +0100

    handle extension functions that are thenable, but need to invoke other member functions

--- test/async-function.js
+++ test/async-function.js
@@ -8,13 +8,6 @@ var expect = chai.expect;
 var chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
 
-// var data = {
-//     foo: [
-//         {bar: 'hello'},
-//         {bar: 'world'}
-//     ]
-// };
-
 var jsonataPromise = function(expr, data, bindings) {
     return new Promise(function(resolve, reject) {
         expr.evaluate(data, bindings, function(error, response) {
@@ -23,14 +16,6 @@ var jsonataPromise = function(expr, data, bindings) {
         });
     });
 };
-
-// var prom = function(arg) {
-//     return new Promise((resolve) => {
-//         setTimeout(function(){
-//             resolve(arg+"Success!");
-//         }, 750);
-//     });
-// };
 
 var httpget = function(url) {
     return new Promise(function(resolve, reject) {
@@ -104,4 +89,74 @@ describe('Invoke JSONata with callback - return values', function() {
         var expr = jsonata('value');
         return expect(jsonataPromise(expr, data)).to.eventually.equal('hello');
     });
+});
+
+describe('Handle chained functions that end in promises', function() {
+
+    var counter = function(count) {
+        return {
+            inc: function() {
+                return counter(count + 1);
+            },
+            then: function(onFulfilled) {
+                setImmediate(function(){
+                    onFulfilled(count);
+                });
+                return Promise.resolve(count);
+            }
+        };
+    };
+
+    var bindings = {
+        counter: counter
+    };
+
+    it('basic function that returns a thenable', function() {
+        var data = {};
+        var expr = jsonata('$counter(5)');
+        return expect(jsonataPromise(expr, data, bindings)).to.eventually.equal(5);
+    });
+
+    it('basic function that returns a thenable, but invokes another function', function() {
+        var data = {};
+        var expr = jsonata('$counter(0).inc()');
+        return expect(jsonataPromise(expr, data, bindings)).to.eventually.equal(1);
+    });
+
+    it('basic function that returns a thenable, but invokes another function several times', function() {
+        var data = {};
+        var expr = jsonata('$counter(0).inc().inc().inc().inc()');
+        return expect(jsonataPromise(expr, data, bindings)).to.eventually.equal(4);
+    });
+
+    it('basic function that returns a thenable and part of a numeric expression', function() {
+        var data = {};
+        var expr = jsonata('$counter(3) + 5');
+        return expect(jsonataPromise(expr, data, bindings)).to.eventually.equal(8);
+    });
+
+    it('basic function that returns a thenable, but invokes another function several times and part of a numeric expression', function() {
+        var data = {};
+        var expr = jsonata('$counter(0).inc().inc().inc().inc() + 3');
+        return expect(jsonataPromise(expr, data, bindings)).to.eventually.equal(7);
+    });
+
+    it('basic function that returns a thenable, but invokes another function - nested', function() {
+        var data = {};
+        var expr = jsonata('$counter($counter(3).inc().inc()).inc()');
+        return expect(jsonataPromise(expr, data, bindings)).to.eventually.equal(6);
+    });
+
+    it('basic function that returns a thenable, then invokes a built-in function', function() {
+        var data = {};
+        var expr = jsonata('$counter(3).inc().$string()');
+        return expect(jsonataPromise(expr, data, bindings)).to.eventually.equal('4');
+    });
+
+    it('basic function that returns a thenable, but invokes a non-existent function', function() {
+        var data = {};
+        var expr = jsonata('$counter(2).inc().foo()');
+        return expect(jsonataPromise(expr, data, bindings)).to.be.rejected;
+    });
+
 });
