05ea292f59a86af8a122600c0534a3418215e4ba
Matt Bailey <mattbaileyuk@users.noreply.github.com>
2018-02-05 10:41:07 +0000

    Update jsonata.js (#144)

--- jsonata.js
+++ jsonata.js
@@ -4653,7 +4653,6 @@ var jsonata = (function() {
     }
 
     // Regular expression to match an ISO 8601 formatted timestamp
-    // Uses https://www.myintervals.com/blog/2009/05/20/iso-8601-date-validation-that-doesnt-suck/ but removes the "week number" format not supported by Date.parse
     var iso8601regex = new RegExp('^\\d{4}(-[01]\\d)*(-[0-3]\\d)*(T[0-2]\\d:[0-5]\\d:[0-5]\\d)*(\\.\\d+)?([+-][0-2]\\d:?[0-5]\\d|Z)?$');
 
     /**
