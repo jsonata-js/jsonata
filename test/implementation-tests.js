d2fc9d5f48ff73597d438382f9fb6439e40f053e
andrew-coleman <andrew_coleman@uk.ibm.com>
2024-02-27 09:11:11 +0000

    Prevent access to __proto__
    Signed-off-by: andrew-coleman <andrew_coleman@uk.ibm.com>

--- test/implementation-tests.js
+++ test/implementation-tests.js
@@ -806,6 +806,26 @@ describe("Tests that are specific to a Javascript runtime", () => {
             });
         });
     });
+    describe("Expressions that attempt to pollute the object prototype", function() {
+        it("should throw an error with __proto__", async function() {
+            const expr = jsonata('{} ~> | __proto__ | {"is_admin": true} |');
+            expect(
+                expr.evaluate()
+            ).to.eventually.be.rejected.to.deep.contain({
+                position: 7,
+                code: "D1010",
+            });
+        });
+        it("should throw an error with __lookupGetter__", async function() {
+            const expr = jsonata('{} ~> | __lookupGetter__("__proto__")() | {"is_admin": true} |');
+            expect(
+                expr.evaluate()
+            ).to.eventually.be.rejected.to.deep.contain({
+                position: 7,
+                code: "D1010",
+            });
+        });
+    });
 });
 
 describe("Test that yield platform specific results", () => {
