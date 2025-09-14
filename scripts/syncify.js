2e6f25c2b4c50b3fe9b2df09b171f2a02dcec1a2
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2025-09-14 15:18:46 +0100

    reasynced

--- scripts/syncify.js
+++ scripts/syncify.js
@@ -17,53 +17,75 @@ srcFiles.forEach(file => {
         .replace(/\/\* async \*\/( ?):/g, "async$1:") // whoops
         .replace(/\bawait\b/g, "/* await */")
         .replace(/\bPromise.all\b/g, "Array.from")
-        .replace(/(\s*)(\bif \(isPromise\(.*?\)\))/g, `\n$1$2 throw new Error(${JSON.stringify(`Expression "$2" evaluated to a promise`.replace('isPromise', ''))});\n$1$2`)
+        .replace(/(\s*)(\bif \(isPromise\(.*?\)\))/g, `\n$1$2 throw new Error(${JSON.stringify(`Check "$2" evaluated to a promise`.replace('isPromise', ''))});\n$1$2`)
         ;
     fs.writeFileSync(path.join(syncDir, path.basename(file)), newContent);
 });
 
 const syncTestDir = path.join(__dirname, "../sync-test");
+
+fs.writeFileSync(path.join(syncTestDir, 'reasynced.js'), 
+` // use the sync library but make it async anyway, so that tests relying on moch-as-promised work
+const syncJsonata = require('../sync/jsonata');
+module.exports = (...args) => {
+    const sync = syncJsonata(...args);
+    return {
+        ...sync,
+        evaluate: async (...args) => {
+            return sync.evaluate(...args);
+        }
+    }
+}
+`);
+
 fs.mkdirSync(syncTestDir, { recursive: true });
 const testFiles = fs.globSync('test/**/*.*')
 
 testFiles.forEach(file => {
-    if (file.includes('async-function.js')) return;
+    if (file.includes('async-function.js')) return; // async behaviour not supported in "sync" mode
     if (fs.statSync(file).isDirectory()) return;
     const newFilepath = file.replace('test/', 'sync-test/')
     const content = fs.readFileSync(file, "utf8");
     let newContent = content
     if (file.endsWith('.js')) {
-        newContent = newContent
-            .replace(/(.)/, `/* eslint-disable */\n$1`)
-            .replaceAll(`../src`, `../sync`)
-        
-        newContent = newContent
-            .split('.be.rejected')
-            .map((part, i, arr) => {
-                if (i === arr.length - 1) return part;
-                const expectParen = 'expect('
-                const indexOfExpectParen = part.lastIndexOf(expectParen);
-                if (indexOfExpectParen === -1) throw new Error(`Expected expect( to appear in ${part}`);
-                const upToLastExpectParen = part.slice(0, indexOfExpectParen);
-                const indent = upToLastExpectParen.slice(upToLastExpectParen.trimEnd().length)
+        newContent = newContent.replace('/src/jsonata', '/sync-test/reasynced')
 
-                const indexOfLastParen = part.lastIndexOf(')');
-                if (indexOfLastParen === -1) throw new Error(`Expected ) to appear in ${part}`);
-                
-                return part.slice(0, indexOfExpectParen)
-                    + `expect(
-                        Promise.resolve().then(() => 
-                            ${part.slice(indexOfExpectParen + expectParen.length, indexOfLastParen).trim()}
-                        )
-                    )`.replaceAll('                    ', indent).replaceAll('\n\n', '\n').trim()
-                    + indent
-                    + part.slice(indexOfLastParen + 1)
-            })
-            .join('.be.rejected')
-            // .join('.to.eventually /* leave this "eventually" alone */.be.rejected')
-
-        // newContent = newContent.replaceAll(`.eventually.`, `/* .eventually */ .`)
+        // these specifically test async behavior, so we skip them
+        const skippables = [
+            `describe("$millis() returns different timestamp for subsequent evaluate() calls"`,
+            `it('should be able to invoke a built-in function passed as an argument'`,
+            `it('should be able to invoke a lambda function passed as an argument'`,
+            `it('should be able to invoke a user-defined function passed as an argument'`,
+        ]
+        for (const s of skippables) {
+            newContent = newContent.replace(s, s.replace('(', '.skip('))
+        }
     }
     fs.mkdirSync(path.dirname(newFilepath), { recursive: true });
     fs.writeFileSync(newFilepath, newContent)
 });
+
+// add one test to make sure we "syncify"ed the library correctly
+fs.writeFileSync(
+    path.join(syncTestDir, 'sync-tests.js'),
+`"use strict";
+
+var jsonata = require("../sync/jsonata");
+var chai = require("chai");
+var expect = chai.expect;
+
+var testdata1 = {
+    "foo": {
+        "bar": 42,
+        "blah": [{"baz": {"fud": "hello"}}, {"baz": {"fud": "world"}}, {"bazz": "gotcha"}],
+        "blah.baz": "here"
+    }, "bar": 98
+};
+
+describe("Sync tests", () => {
+    it("should evaluate a simple expression without awaiting", () => {
+        expect(jsonata("foo.bar").evaluate(testdata1)).to.equal(42);
+    });
+});
+`
+)
